f5dcb17245b31b8bbff7fae539d5a75544dbf2e0
Nesaak <52047222+Nesaak@users.noreply.github.com>
2020-05-27 20:19:16 -0400

    init

---
+++ src/main/java/com/voltskiya/core/Voltskiya.java
@@ -0,0 +1,11 @@
+package com.voltskiya.core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Voltskiya extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+}

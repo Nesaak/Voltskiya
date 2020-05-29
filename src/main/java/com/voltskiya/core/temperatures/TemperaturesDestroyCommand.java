e05f4a063a659c7a769916432f9ad29c31ea91c4
amp7368 <41219847+amp7368@users.noreply.github.com>
2020-05-29 14:17:38 -0400

    changed the TemperaturesDestroyCommand to use acf

--- src/main/java/com/voltskiya/core/temperatures/TemperaturesDestroyCommand.java
+++ src/main/java/com/voltskiya/core/temperatures/TemperaturesDestroyCommand.java
@@ -1,37 +1,22 @@
 package com.voltskiya.core.temperatures;
 
+import co.aikar.commands.BaseCommand;
+import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.Single;
+import co.aikar.commands.annotation.Subcommand;
 import com.voltskiya.core.temperatures.constants.NavigatePlayers;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.PluginCommand;
 import org.bukkit.entity.Player;
 import org.bukkit.persistence.PersistentDataContainer;
-import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
-public class TemperaturesDestroyCommand implements CommandExecutor {
-    private final JavaPlugin plugin;
-
-    public TemperaturesDestroyCommand(JavaPlugin plugin) {
-        this.plugin = plugin;
-        PluginCommand command = plugin.getCommand("temperature_remove");
-        if (command == null) {
-            System.err.println(NavigatePlayers.PLUGIN_NAME + " could not get command: temperature_remove");
-            return;
-        }
-        command.setExecutor(this);
-    }
-
-    @Override
-    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
-        if (args.length != 1) {
-            commandSender.sendMessage("Include only the player's name in your command arguments.");
-            return false;
-        }
-        Player player = Bukkit.getPlayer(args[0]);
+@CommandAlias("temperature")
+public class TemperaturesDestroyCommand extends BaseCommand {
+    @Subcommand("remove")
+    public boolean removeTemp(@NotNull CommandSender commandSender, @Single String playerName) {
+        Player player = Bukkit.getPlayer(playerName);
         if (player == null) {
             commandSender.sendMessage("That player does not exist right now.");
             return false;
---
+++ src/main/java/com/voltskiya/core/temperatures/TemperaturesModule.java
@@ -0,0 +1,34 @@
+package com.voltskiya.core.temperatures;
+
+
+import com.voltskiya.core.Voltskiya;
+import com.voltskiya.core.VoltskiyaModule;
+import com.voltskiya.core.temperatures.constants.NavigateArmor;
+import com.voltskiya.core.temperatures.constants.NavigateBlocks;
+import com.voltskiya.core.temperatures.constants.NavigatePlayers;
+import com.voltskiya.core.temperatures.constants.NavigatePotions;
+import com.voltskiya.core.temperatures.constants.biomes.NavigateBiomes;
+import com.voltskiya.core.temperatures.constants.results.NavigateResults;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TemperaturesModule extends VoltskiyaModule {
+    public void enabled() {
+        JavaPlugin plugin = Voltskiya.get();
+        NavigatePlayers.initialize(plugin);
+        NavigateBiomes.initialize(plugin);
+        NavigateBlocks.initialize(plugin);
+        NavigateArmor.initialize(plugin);
+        NavigatePotions.initialize(plugin);
+        NavigateResults.initialize(plugin);
+        ActionBar.initialize(plugin);
+        IsWetCheck.initialize(plugin);
+        new PlayerListener(plugin);
+        new TemperaturesReload(plugin);
+        Voltskiya.get().getCommandManager().registerCommand(new TemperaturesDestroyCommand());
+    }
+
+    @Override
+    public String getName() {
+        return "Temperatures";
+    }
+}
